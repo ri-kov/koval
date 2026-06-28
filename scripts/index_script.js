de6d9d98847745b93ea43cece2a2977d5c284e49
ri-kov <rkovalenko1709@gmail.com>
2026-06-27 20:17:24 -0500

    js

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -378,34 +378,3 @@ repairBtmButton.addEventListener('mouseleave', () => {
     repairBtmButton.style.color = '#ffffff';
     repairBtmIcon.style.color = '#ffffff';
 });
-
-const modCustomSelect = document.getElementById("modConditionSelect");
-const modTrigger = modCustomSelect.querySelector(".mod_select_trigger");
-const modSelectedText = modCustomSelect.querySelector(".mod_selected_text");
-const modOptions = modCustomSelect.querySelectorAll(".mod_select_options li");
-const modHiddenInput = document.getElementById("modSelServ");
-
-modTrigger.addEventListener("click", () => {
-  modCustomSelect.classList.toggle("exp");
-});
-
-modOptions.forEach(option => {
-  option.addEventListener("click", () => {
-    modSelectedText.textContent = option.textContent;
-    modHiddenInput.value = option.dataset.value;
-    modSelectedText.style.color = 'var(--bs-body-color)';
-    modSelectedText.style.fontWeight = '450';
-    modSelectedText.style.fontSize = '1rem';
-
-    modOptions.forEach(item => item.classList.remove("selected"));
-    option.classList.add("selected");
-
-    modCustomSelect.classList.remove("exp");
-  });
-});
-
-document.addEventListener("click", event => {
-  if (!modCustomSelect.contains(event.target)) {
-    modCustomSelect.classList.remove("exp");
-  }
-});
---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const modCustomSelect = document.getElementById("modConditionSelect");
+const modTrigger = modCustomSelect.querySelector(".mod_select_trigger");
+const modSelectedText = modCustomSelect.querySelector(".mod_selected_text");
+const modOptions = modCustomSelect.querySelectorAll(".mod_select_options li");
+const modHiddenInput = document.getElementById("modSelServ");
+
+modTrigger.addEventListener("click", () => {
+  modCustomSelect.classList.toggle("exp");
+});
+
+modOptions.forEach(option => {
+  option.addEventListener("click", () => {
+    modSelectedText.textContent = option.textContent;
+    modHiddenInput.value = option.dataset.value;
+    modSelectedText.style.color = 'var(--bs-body-color)';
+    modSelectedText.style.fontWeight = '450';
+    modSelectedText.style.fontSize = '1rem';
+
+    modOptions.forEach(item => item.classList.remove("selected"));
+    option.classList.add("selected");
+
+    modCustomSelect.classList.remove("exp");
+  });
+});
+
+document.addEventListener("click", event => {
+  if (!modCustomSelect.contains(event.target)) {
+    modCustomSelect.classList.remove("exp");
+  }
+});
+
