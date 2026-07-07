f166644c964462760e974e86ff221d8dad3cc1b7
ri-kov <rkovalenko1709@gmail.com>
2026-07-07 13:44:41 -0500

    js

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -408,10 +408,22 @@ hamburger.addEventListener("click", () => {
     mobileMenu.classList.add("active");
     hamburger.style.display = "none";
     closeMenu.style.display = "block";
+    document.body.classList.add("menu_open");
 });
 
 closeMenu.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
     hamburger.style.display = "block";
     closeMenu.style.display = "none";
+    document.body.classList.remove("menu_open");
+});
+
+document.querySelectorAll(".nav_links a").forEach((link) => {
+    link.addEventListener("click", () => {
+        mobileMenu.classList.remove("active");
+        document.body.classList.remove("menu_open");
+
+        hamburger.style.display = "block";
+        closeMenu.style.display = "none";
+    });
 });
