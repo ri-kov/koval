b12b4c6842af31c2402a5c707ed27b36fdbf0de6
ri-kov <rkovalenko1709@gmail.com>
2026-08-25 13:06:47 -0500

    s

--- scripts/script.js
+++ scripts/script.js
@@ -366,3 +366,31 @@ const modCancel = document.getElementById("modCancel");
 modCancel.addEventListener("click", () => {
     resetRepairBooking();
 });
+
+const hamburger = document.getElementById("hamburger");
+const closeMenu = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+    hamburger.style.display = "none";
+    closeMenu.style.display = "block";
+    document.body.classList.add("menu_open");
+});
+
+closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    hamburger.style.display = "block";
+    closeMenu.style.display = "none";
+    document.body.classList.remove("menu_open");
+});
+
+document.querySelectorAll(".nav_links a").forEach((link) => {
+    link.addEventListener("click", () => {
+        mobileMenu.classList.remove("active");
+        document.body.classList.remove("menu_open");
+
+        hamburger.style.display = "block";
+        closeMenu.style.display = "none";
+    });
+});
