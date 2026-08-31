28a28d91277cafadf4998cdb9386eacf6ed76c8f
ri-kov <rkovalenko1709@gmail.com>
2026-08-31 13:34:59 -0500

    class

--- backend/src/main/java/com/valenauto/backend/HelloController.java
+++ backend/src/main/java/com/valenauto/backend/HelloController.java
@@ -2,10 +2,24 @@ package com.valenauto.backend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from backend";
+    }
+
+    public Map<String, Object> getCar() {
+        Map<String, Object> car = new HashMap<>();
+
+        car.put("make", "Mazda");
+        car.put("model", "Mazda 3");
+        car.put("year", "2018");
+        car.put("price", 12500);
+
+        return car;
     }
 }
---
+++ backend/src/main/java/com/valenauto/backend/Vehicle.java
@@ -0,0 +1,4 @@
+package com.valenauto.backend;
+
+public class Vehicle {
+}
