a68df624428451dd339b51a73e9b5cd97db39f08
ri-kov <rkovalenko1709@gmail.com>
2026-08-31 13:02:30 -0500

    test

--- backend/src/main/java/com/valenauto/backend/HelloController.java
+++ backend/src/main/java/com/valenauto/backend/HelloController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        
-    }
         return "Hello from backend";
-
+    }
 }
