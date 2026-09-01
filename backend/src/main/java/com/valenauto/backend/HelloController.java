aab4c06ae04846c338c7633adba715b8591470cf
ri-kov <rkovalenko1709@gmail.com>
2026-08-31 19:50:44 -0500

    java

--- backend/src/main/java/com/valenauto/backend/HelloController.java
+++ backend/src/main/java/com/valenauto/backend/HelloController.java
@@ -2,6 +2,9 @@ package com.valenauto.backend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
     @GetMapping("/api/hello")
@@ -9,6 +12,7 @@ public class HelloController {
         return "Hello from backend";
     }
 
+    @GetMapping("/api/car")
     public Vehicle getCar() {
         Vehicle car = new Vehicle(
                 "Mazda",
@@ -20,4 +24,36 @@ public class HelloController {
 
         return car;
     }
+
+    @GetMapping("/api/vehicles")
+    public List<Vehicle> getVehicles() {
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        vehicles.add(new Vehicle (
+                "Mazda",
+                "Mazda 3",
+                2018,
+                192500,
+                12500
+        ));
+
+        vehicles.add(new Vehicle (
+                "Honda",
+                "Civic",
+                2015,
+                180000,
+                9500
+        ));
+
+        vehicles.add(new Vehicle (
+                "Subaru",
+                "Legacy",
+                2016,
+                160000,
+                11000
+        ));
+
+        return vehicles;
+    }
+
 }
