f78540e443be11f36821aa10f5c03c60a148fb85
ri-kov <rkovalenko1709@gmail.com>
2026-09-02 15:58:40 -0500

    a

--- backend/src/main/java/com/valenauto/backend/Vehicle.java
+++ backend/src/main/java/com/valenauto/backend/Vehicle.java
@@ -1,7 +1,15 @@
 package com.valenauto.backend;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String make;
     private String model;
     private int year;
