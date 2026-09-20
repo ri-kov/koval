c41e5acf080d243a0dd47761703e091c15881ea2
ri-kov <rkovalenko1709@gmail.com>
2026-09-20 19:22:21 -0400

    thumb

--- vehicles/src/app/app.ts
+++ vehicles/src/app/app.ts
@@ -28,6 +28,20 @@ export class App {
     title: 'Clean Title',
     owners: 1,
     description: 'This 2019 Hyundai Sonata LE is a reliable, comfortable, and fuel-efficient sedan with low kilometres. Well maintained and in great overall condition, it offers a smooth ride, modern features, and excellent value. This vehicle has been safety inspected, comes with a clean title, and is ready for its next owner. Ideal for commuting, family use, or anyone looking for a dependable sedan.',
-    carImages: []
+    carImages: [
+      'pictures/cars/2019sonatasample/1.JPG',
+      'pictures/cars/2019sonatasample/2.JPG',
+      'pictures/cars/2019sonatasample/3.JPG',
+      'pictures/cars/2019sonatasample/4.JPG',
+      'pictures/cars/2019sonatasample/5.JPG',
+      'pictures/cars/2019sonatasample/6.JPG',
+      'pictures/cars/2019sonatasample/7.JPG',
+    ]
+  };
+
+  selectedImage = this.vehicle.carImages[0];
+
+  selectImage(image: string) {
+    this.selectedImage = image;
   }
 }
