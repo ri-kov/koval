2c5bf264250ed04e468c55773e170c99a3dc14e4
ri-kov <rkovalenko1709@gmail.com>
2026-09-20 18:44:53 -0400

    interface

---
+++ vehicles/src/app/vehicle.ts
@@ -0,0 +1,20 @@
+export interface Vehicle {
+    year: number;
+    make: string;
+    model: string;
+    trim: string;
+    price: number;
+    mileage: number;
+    engine: string;
+    transmission: string;
+    fuel: string;
+    drivetrain: string;
+    extColor: string;
+    intColor: string;
+    seats: number;
+    certification: string;
+    title: string;
+    owners: number;
+    description: string;
+    carImages: string[];
+}
